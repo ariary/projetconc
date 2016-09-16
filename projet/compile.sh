190eb144862e13c34b21c21a242d26790c4c1122
ntoine_rabenandrasana <antoine.rabenandrasa@etu.unice.fr>
2016-09-16 17:24:56 +0200

    Raemplissage du compile.sh

---
+++ projet/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\nSi vous voulez compiler le simulateur sans partie graphique entrez 'sans' \nSinon entrez 'avec'"
+read -n 5 pgrah
+if [ $pgraph = "avec" ]
+then
+	make avec
+else
+	make sans
+fi
+
+#j'aurais juste pu faire make $praph mais le nom de make avec par exemple peut changer
