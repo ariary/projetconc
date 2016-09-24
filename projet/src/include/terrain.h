12f40e9321ed4db4c133d17dde8033accb4f1cee
ntoine_rabenandrasana <antoine.rabenandrasa@etu.unice.fr>
2016-09-24 12:54:54 +0200

    structuration 3

---
+++ projet/src/include/terrain.h
@@ -0,0 +1,11 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+typedef struct Terrain{
+	int** taleau;
+	int nb_personnes;
+	struct Personne* tableau_personnes;
+}Terrain;
+
+
+void init(int nb_personnes, int nb_threads);//init les personnes sur le terrain avec le nombre de thread indiqué
+#endif
