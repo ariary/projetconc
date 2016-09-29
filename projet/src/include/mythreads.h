e0d789e15dbfbcb17f7d3c21f5c92a3cbb64842a
Antoine Rabenandrasana <antoine.rabenandrasana@etu.unice.fr>
2016-09-29 16:34:01 +0200

    Correction: header mythreads (commit precedent .cpp)

---
+++ projet/src/include/mythreads.h
@@ -0,0 +1,26 @@
+/*
+ * thread.h
+ *
+ *  Created on: 24 sept. 2016
+ *      Author: Adrien Prestini & Antoine Rabenandrasana
+ */
+
+#ifndef MYTHREADS_H_
+#define MYTHREADS_H_
+
+
+#include "personne.h"
+#include "terrain.h"
+
+/*t0*/
+void *thread_avancerALL (void *p_data); 
+
+/*t1*/
+void *thread_avancerNE(void *pd_data);
+void *thread_avancerNO(void *pd_data);
+void *thread_avancerSE(void *pd_data);
+void *thread_avancerSO(void *pd_data);
+
+/*t2*/
+void *thread_avancerALONE(void *pd_data);
+#endif
